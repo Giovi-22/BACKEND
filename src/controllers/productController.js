4d1cd8576cf6327ed6a3f7a807a6d2e0f5eb05ab
Giovanni <giovannibarolin@gmail.com>
2023-04-26 18:34:45 -0300

    feat: creating methods for the http requests

---
+++ src/controllers/productController.js
@@ -0,0 +1,55 @@
+import ProductManager from '../manager/ProductManager.js';
+
+class ProductController{
+
+    addProduct = async (req,res)=>{
+            const product = req.body;
+            try {
+                const pManager = new ProductManager();
+                const newProduct = await pManager.add(product);
+                res.status('201').json({status:'success',data:newProduct});
+            } catch (error) {
+                res.status('500').json({status:'error',message:`Se ha producido un error: ${error.message}`});
+                console.log(`Se ha producido un error: ${error.message}`);
+            }
+    }
+
+    getProduct = async (req,res)=>{
+        const pid = req.params.pid;
+        try {
+            const pManager = new ProductManager();
+            const product = await pManager.getOne(pid);
+            res.status('200').json({status:'success',data:product});
+        } catch (error) {
+            res.status('500').json({status:'error',message:`Se ha producido un error: ${error.message}`});
+            console.log(`Se ha producido un error: ${error.message}`);
+        }
+    }
+
+    updateProduct = async (req,res)=>{
+        const pid = req.params.pid;
+        try {
+            const pManager = new ProductManager();
+            const productUpdated = await pManager.updateOne(pid);
+            res.status('200').json({status:'success',data:productUpdated});
+        } catch (error) {
+            res.status('500').json({status:'error',message:`Se ha producido un error: ${error.message}`});
+            console.log(`Se ha producido un error: ${error.message}`);
+        }
+    }
+
+    deleteProduct = async (req,res)=>{
+        const pid = req.params.pid;
+        try {
+            const pManager = new ProductManager();
+            const productDeleted = await pManager.deleteOne(pid);
+            res.status('200').json({status:'success',data:productDeleted});
+        } catch (error) {
+            res.status('500').json({status:'error',message:`Se ha producido un error: ${error.message}`});
+            console.log(`Se ha producido un error: ${error.message}`);
+        }
+    }
+
+}
+
+export default ProductController;
